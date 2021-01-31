978b78fe0167e3e3d75ab9286b95131aca9bddf7
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-01-31 23:00:57 +0000

    added settimeout

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ let drums = document.querySelectorAll('.drum');
 
 console.log(drums);
 
-for (let i = 0; i < drums.length; i++) {
-	drums[i].onclick = () => {
-		console.log(drums[i].innerHTML);
-		let sound = 'sounds/tom-' + drums[i].innerHTML + '.mp3';
-		new Audio(sound).play();
-	};
-}
+// for (let i = 0; i < drums.length; i++) {
+// 	drums[i].onclick = () => {
+// 		console.log(drums[i].innerHTML);
+// 		let sound = 'sounds/tom-' + drums[i].innerHTML + '.mp3';
+// 		new Audio(sound).play();
+// 	};
+// }
 
 // for (let i = 0; i < drums.length; i++) {
 // 	drums[i].onkeypress = (e) => {
@@ -24,3 +24,17 @@ for (let i = 0; i < drums.length; i++) {
 // 		new Audio(sound).play();
 // 	};
 // }
+
+for (let i = 0; i < drums.length; i++) {
+	drums[i].onkeypress = (e) => {
+		// console.log(drums[i].innerHTML);
+		console.log(e);
+		let sound = 'sounds/tom-' + e.key + '.mp3';
+		new Audio(sound).play();
+		drums[i].className = 'pressed';
+		setTimeout(() => {
+			drums[i].className = e.key + ' drum';
+		}, 1000);
+		// console.log(drums[i].className);
+	};
+}
